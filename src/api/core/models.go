95152b1b7fcda5267760acdbb0a36cbe7522060f
scaldarola <sebastian.caldarola@mercadolibre.com>
2020-01-23 10:57:56 -0300

    working post

--- src/api/core/models.go
+++ src/api/core/models.go
@@ -4,7 +4,7 @@ import "github.com/sebastian/location-backend/src/api/utils"
 
 type Location struct {
 	Model     utils.Model
-	Latitude  float64 `json:"latitude"`
-	Longitude float64 `json:"longitude"`
+	Latitude  string `json:"latitude"`
+	Longitude string `json:"longitude"`
 	DriverId  int     `json:"driver_id"`
 }
